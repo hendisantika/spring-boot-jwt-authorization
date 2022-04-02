813058af4d1b900a742650bc602cd5bb959022f3
Hendi Santika <hendisantika@gmail.com>
2022-04-02 18:42:55 +0700

    #14 - Add configure in SecurityConfiguration

--- src/main/java/com/hendisantika/config/SecurityConfiguration.java
+++ src/main/java/com/hendisantika/config/SecurityConfiguration.java
@@ -3,7 +3,9 @@ package com.hendisantika.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -37,4 +39,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests(configurer ->
+                        configurer
+                                .antMatchers(
+                                        "/error",
+                                        "/login"
+                                )
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                );
+
+        // JWT Validation Configuration
+        http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(authenticationConverter());
+    }
 }
