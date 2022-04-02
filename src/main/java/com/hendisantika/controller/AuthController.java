7a55d51f61b6d83c492340bab3ef56eb7825690f
Hendi Santika <hendisantika@gmail.com>
2022-04-02 18:47:11 +0700

    #23 - Add AuthController

---
+++ src/main/java/com/hendisantika/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.config.JwtHelper;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-authorization
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/04/22
+ * Time: 18.46
+ */
+@CrossOrigin(origins = {"${app.security.cors.origin}"})
+@RestController
+public class AuthController {
+
+    private final JwtHelper jwtHelper;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(JwtHelper jwtHelper, UserDetailsService userDetailsService,
+                          PasswordEncoder passwordEncoder) {
+        this.jwtHelper = jwtHelper;
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+}
