da1217ded220fd97cf378adfe24052661a51270e
Hendi Santika <hendisantika@gmail.com>
2022-04-02 18:41:10 +0700

    #11 - Add createJwtForClaims in JwtHelper

--- src/main/java/com/hendisantika/config/JwtHelper.java
+++ src/main/java/com/hendisantika/config/JwtHelper.java
@@ -1,10 +1,17 @@
 package com.hendisantika.config;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,5 +32,22 @@ public class JwtHelper {
     public JwtHelper(RSAPrivateKey privateKey, RSAPublicKey publicKey) {
         this.privateKey = privateKey;
         this.publicKey = publicKey;
+    }
+
+    public String createJwtForClaims(String subject, Map<String, String> claims) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(Instant.now().toEpochMilli());
+        calendar.add(Calendar.DATE, 1);
+
+        JWTCreator.Builder jwtBuilder = JWT.create().withSubject(subject);
+
+        // Add claims
+        claims.forEach(jwtBuilder::withClaim);
+
+        // Add expiredAt and etc
+        return jwtBuilder
+                .withNotBefore(new Date())
+                .withExpiresAt(calendar.getTime())
+                .sign(Algorithm.RSA256(publicKey, privateKey));
     }
 }
