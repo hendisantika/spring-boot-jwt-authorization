fd2b306ea103d9916ba90d7adaf6485c1a4406f6
Hendi Santika <hendisantika@gmail.com>
2022-04-02 18:48:19 +0700

    #25 - Add ProductController

---
+++ src/main/java/com/hendisantika/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-authorization
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/04/22
+ * Time: 18.47
+ */
+@RestController
+@RequestMapping("products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+}
