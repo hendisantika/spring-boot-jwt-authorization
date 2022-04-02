8cce5071d0742c4d19984004676aea9baeaeb364
Hendi Santika <hendisantika@gmail.com>
2022-04-02 18:47:46 +0700

    #24 - Add login API in AuthController

--- src/main/java/com/hendisantika/controller/AuthController.java
+++ src/main/java/com/hendisantika/controller/AuthController.java
@@ -1,10 +1,24 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.config.JwtHelper;
+import com.hendisantika.config.SecurityConfiguration;
+import com.hendisantika.controller.resource.LoginResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +42,34 @@ public class AuthController {
         this.jwtHelper = jwtHelper;
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping(path = "login", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public LoginResult login(
+            @RequestParam String username,
+            @RequestParam String password) {
+
+        UserDetails userDetails;
+        try {
+            userDetails = userDetailsService.loadUserByUsername(username);
+        } catch (UsernameNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
+        }
+
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            Map<String, String> claims = new HashMap<>();
+            claims.put("username", username);
+
+            String authorities = userDetails.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.joining(" "));
+            claims.put(SecurityConfiguration.AUTHORITIES_CLAIM_NAME, authorities);
+            claims.put("userId", String.valueOf(1));
+
+            String jwt = jwtHelper.createJwtForClaims(username, claims);
+            return new LoginResult(jwt);
+        }
+
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not authenticated");
     }
 }
